7ab583c0fb2bf7524c4f6b4a6d550eb9506a1f7c
MattLD13 <mdicks2025@pingry.org>
2024-03-05 17:42:24 -0500

    ads

--- src/main/java/frc/robot/subsystems/Blinkin.java
+++ src/main/java/frc/robot/subsystems/Blinkin.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems;
 
+import frc.robot.Constants.MechanismConstants;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.MechanismConstants;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkBase.SoftLimitDirection;
-import com.revrobotics.SparkPIDController;
 
 public class Blinkin extends SubsystemBase {
+    Spark blinkin;
+
+    public Blinkin() {
+        blinkin = new Spark(Constants.LightsPWM));
+    }
 }
