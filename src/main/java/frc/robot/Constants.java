ae55185753f7eed86e932a21222f41d729c3cdd1
MattLD13 <mdicks2025@pingry.org>
2024-03-06 14:36:14 -0500

    MORE SPEED

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -198,6 +198,6 @@ public final class Constants {
   }
 
   public static final class NeoMotorConstants {
-    public static final double kFreeSpeedRpm = 6704;
+    public static final double kFreeSpeedRpm = 6784;
   }
 }
