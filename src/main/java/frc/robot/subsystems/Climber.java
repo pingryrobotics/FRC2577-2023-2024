11dff54f6c8d411f3b43428f6c7a9cff275d5919
Sia Ghatak <sghatak2025@pingry.org>
2024-03-09 08:13:48 -0500

    jame from sia

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,45 +1,71 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+<<<<<<< Updated upstream
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.MechanismConstants;
-
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase.ControlType;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkBase.SoftLimitDirection;
-import com.revrobotics.SparkPIDController;
-
 public class Climber extends SubsystemBase {
-    private CANSparkMax climberLeft;
-    private CANSparkMax climberRight;
-
-	/**
-	 * Creates a new ExampleSubsystem.
-	 */
-    public Climber(CANSparkMax left, CANSparkMax right) {
-        this.climberLeft = left;
-        this.climberRight = right;
-        climberLeft.setInverted(true);
-        climberRight.setInverted(false);
-		climberLeft.setIdleMode(IdleMode.kBrake);
-        climberRight.setIdleMode(IdleMode.kBrake);
 	}
 
 	@Override
+=======
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.Rev2mDistanceSensor;
+import com.revrobotics.CANSparkMax.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ExampleSubsystem extends SubsystemBase {
+	private DoubleSolenoid clawSolenoid;
+	private Compressor compressor;
+	private boolean state = false;
+	/**
+	 * Creates a new ExampleSubsystem.
+	 */
+	public Claw(DoubleSolenoid clawSolenoid) {
+		this.clawSolenoid = clawSolenoid;
+		this.isShutoff = true;
+		compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+		this.clawSolenoid.set(DoubleSolenoid.Value.kForward);
+		// compressor.disable();
+		// this.setDefaultCommand(new ClawStop(this));
+		SmartDashboard.putBoolean("Has closed", false);
+		
+	}
+	@Override
+
+>>>>>>> Stashed changes
 	public void periodic() {
 		// This method will be called once per scheduler run
 	}
 
+<<<<<<< Updated upstream
 	public void setSpeed(double speed) {
         climberLeft.set(speed);
         climberRight.set(speed);
+=======
+	public void enableCompressor() {
+		compressor.enableDigital();
+	}
+
+	public void disableCompressor() {
+		compressor.disable();
+	}
+
+	public void up() {
+		// singleSolenoid.set(true);
+		// clawSolenoid.toggle();
+		clawSolenoid.set(DoubleSolenoid.Value.kForward);
+		state = false;
+>>>>>>> Stashed changes
 	}
 }
